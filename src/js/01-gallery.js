72c871353db483516baccd768998f3b9aed93ed9
KaterinaBondarenko <kat9bondarenko@gmail.com>
2021-10-25 19:16:38 +0300

    summary

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery'); // Поиск элементов
+const imageMarkup = addImagesMarkup(galleryItems); // переменная для разметки
+
+galleryEl.insertAdjacentHTML('beforeend', imageMarkup);
+
+function addImagesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img
+       class="gallery__image"
+      src="${preview}"
+       alt="${description}" />
+    </a>`;
+    })
+    .join('');
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
+gallery.on('show.simplelightbox');
