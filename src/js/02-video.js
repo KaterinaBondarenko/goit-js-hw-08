05ba4b1c997023b7d2275b826611d52182a2695b
KaterinaBondarenko <kat9bondarenko@gmail.com>
2021-10-25 23:34:26 +0300

    summary

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+
+const onPlay = new Player(iframeEl, {
+  duration: 61.857,
+  percent: 0.049,
+  seconds: 3.034,
+});
