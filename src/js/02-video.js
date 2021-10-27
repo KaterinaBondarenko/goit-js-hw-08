65abfb4f197ec49ef603b4b9bc9c809ea1493c54
KaterinaBondarenko <kat9bondarenko@gmail.com>
2021-10-27 15:56:00 +0300

    sum

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,40 +2,39 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
-const savedseconds = localStorage.getItem('LOCALSTORAGE_KEY');
-const parsedSettings = JSON.parse(savedseconds);
-console.log(parsedSettings); // why null?
-
 const iframeEl = document.querySelector('#vimeo-player');
 
 const player = new Player(iframeEl);
 
 //Вызов воспроизведения видео
-player.on('play', setTime);
+player.on('play', function () {
+  console.log('played the video!');
+});
 
 // Указывает время воспроизвидения и записывает в локал хост!!!!!!!!!!
 player.on('timeupdate', function (evt) {
   const currentTime = evt.seconds;
   localStorage.setItem(LOCALSTORAGE_KEY, currentTime);
-  console.log('seconds:', currentTime);
+  // console.log('seconds:', currentTime);
 });
 
-function setTime(parsedSettings) {
-  player.setCurrentTime(parsedSettings);
-}
+// const savedseconds = localStorage.getItem('LOCALSTORAGE_KEY');
+// const parsedSettings = JSON.parse(savedseconds);
+// console.log(parsedSettings); // why null?
 
-// player.setCurrentTime();
-// //   .then(function (seconds) {
-//     console.log(seconds);
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
+player
+  .setCurrentTime(30)
+  .then(function (seconds) {
+    console.log(seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
 
-//       default:
-//         // some other error occurred
-//         break;
-//     }
-//   });
+      default:
+        // some other error occurred
+        break;
+    }
+  });
