fd24e970b8d222b54ea40158c98c481d82768719
KaterinaBondarenko <kat9bondarenko@gmail.com>
2021-10-27 15:40:50 +0300

    add 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,41 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const savedseconds = localStorage.getItem('LOCALSTORAGE_KEY');
+const parsedSettings = JSON.parse(savedseconds);
+console.log(parsedSettings); // why null?
+
 const iframeEl = document.querySelector('#vimeo-player');
 
-const onPlay = new Player(iframeEl, {
-  duration: 61.857,
-  percent: 0.049,
-  seconds: 3.034,
+const player = new Player(iframeEl);
+
+//Вызов воспроизведения видео
+player.on('play', setTime);
+
+// Указывает время воспроизвидения и записывает в локал хост!!!!!!!!!!
+player.on('timeupdate', function (evt) {
+  const currentTime = evt.seconds;
+  localStorage.setItem(LOCALSTORAGE_KEY, currentTime);
+  console.log('seconds:', currentTime);
 });
+
+function setTime(parsedSettings) {
+  player.setCurrentTime(parsedSettings);
+}
+
+// player.setCurrentTime();
+// //   .then(function (seconds) {
+//     console.log(seconds);
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,61 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+// const formEl = document.querySelector('.feedback-form');
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form  textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 200));
+
+// populateTextarea();
+
+/*
+ * - Останавливаем поведение по умолчанию
+ * - Убираем сообщение из хранилища
+ * - Очищаем форму
+ */
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log('Отправляем форму');
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+/*
+ * - Получаем значение поля
+ * - Сохраняем его в хранилище
+ * - Можно добавить throttle
+ */
+function onTextareaInput(evt) {
+  const message = evt.target.value;
+
+  localStorage.setItem(LOCALSTORAGE_KEY, message);
+}
+
+/*
+ * - Получаем значение из хранилища
+ * - Если там что-то было, обновляем DOM
+ */
+// function populateTextarea() {
+//   const savedMessage = localStorage.getItem(LOCALSTORAGE_KEY);
+
+//   if (savedMessage) {
+//     refs.textarea.value = savedMessage;
+//   }
+// }
+
+// const formData = {};
+
+// refs.form.addEventListener('input', e => {
+//   // console.log(e.target.name);
+//   // console.log(e.target.value);
+
+//   formData[e.target.name] = e.target.value;
+
+//   console.log(formData);
+// });
