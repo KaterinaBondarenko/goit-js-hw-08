524ecbcd8d521a19d5e30fa0aa8139ab7c7e3d28
KaterinaBondarenko <kat9bondarenko@gmail.com>
2021-10-28 12:26:17 +0300

    add 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,38 +3,16 @@ import throttle from 'lodash.throttle';
 
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 const iframeEl = document.querySelector('#vimeo-player');
-
 const player = new Player(iframeEl);
 
-//Вызов воспроизведения видео
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('play', onReadTime);
+player.on('timeupdate', throttle(onSaveTime, 1000));
 
-// Указывает время воспроизвидения и записывает в локал хост!!!!!!!!!!
-player.on('timeupdate', function (evt) {
-  const currentTime = evt.seconds;
-  localStorage.setItem(LOCALSTORAGE_KEY, currentTime);
-  // console.log('seconds:', currentTime);
-});
+function onSaveTime(evt) {
+  const saveTime = evt.seconds;
+  localStorage.setItem(LOCALSTORAGE_KEY, saveTime);
+}
 
-// const savedseconds = localStorage.getItem('LOCALSTORAGE_KEY');
-// const parsedSettings = JSON.parse(savedseconds);
-// console.log(parsedSettings); // why null?
-
-player
-  .setCurrentTime(30)
-  .then(function (seconds) {
-    console.log(seconds);
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+function onReadTime() {
+  player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+}
